e7b471bdc6bffa3295c3b5174d87a08bc1404392
yeonju lee <yeonju.lee1005@gmail.com>
2021-09-12 23:47:52 +0900

    JS commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Js_Learn
+
+
+## Chrome Browser Clone
---
+++ app.js
@@ -0,0 +1,27 @@
+
+const calculator = {
+    add: function(num1,num2) {
+        console.log(num1 + num2);
+    },
+    minus: function(num1,num2) {
+        console.log(num1 - num2);
+    },
+    multi: function(num1,num2) {
+        console.log(num1*num2);
+    },
+    devide: function(num1,num2) {
+        console.log(num1/num2);
+    },
+    powerof: function(num1,num2) {
+        console.log(num1**num2);
+    }
+}
+
+
+
+
+calculator.add(1,2);
+calculator.minus(11,33);
+calculator.multi(66,11);
+calculator.devide(11,22);
+calculator.powerof(22,44);
