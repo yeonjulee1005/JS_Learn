47c7d89d8bb27bcf797513f7b2cd08768a98e306
yeonju lee <yeonju.lee1005@gmail.com>
2021-09-13 23:44:22 +0900

    JS Study

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-
-const calculator = {
-    add: function(num1,num2) {
-        return num1 + num2;
-    },
-    minus: function(num1,num2) {
-        return num1 - num2;
-    },
-    multi: function(num1,num2) {
-        return num1 * num2;
-    },
-    devide: function(num1,num2) {
-        return num1 / num2;
-    },
-    powerof: function(num1,num2) {
-        return num1 ** num2;
-    }
-}
+const title =document.querySelector(".hello h1");
 
 
 
-const plusResult = calculator.add(2,44);
-const minusResult = calculator.minus(2,44);
-const muntiResult = calculator.multi(3,plusResult);
-const devideResult = calculator.devide(muntiResult, 44);
-const powerOfResult = calculator.powerof(plusResult, devideResult);
+console.log(title);
