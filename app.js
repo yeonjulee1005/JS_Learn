6e5f0b5137f4728f98f6cd8a5fba2f4bee9bf8df
yeonju lee <yeonju.lee1005@gmail.com>
2021-09-14 23:56:13 +0900

    JS Study

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ function onLoginSubmit(event) {
     event.preventDefault(); // 기본적인 브라우저의 동작을 막는 함수 Form의 자동 새로고침 방지
     loginForm.classList.add(HIDDEN_CLASSNAME); // #loginForm에 .hidden 를 추가하는 것, greeting은 보이게 form은 안보이게!
     const username = loginInput.value; // <input> 에 value를 넣는다! 사용자 이름
-    localStorage.setItem(USERNAME_KEY, username); // greeting.innerText = "hello " + username;
+    localStorage.setItem(USERNAME_KEY, username); // 로컬스토리지에 key:username , value:사용자이름 으로 저장
     paintGreetings(username);
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `hello ${username}`;  // #greeting에 text와 양식으로 받은 이름 표기!
+    greeting.innerText = `hello ${username}`;  // #greeting에 text와 username으로 받은내용 작성!
     greeting.classList.remove(HIDDEN_CLASSNAME);   // 로컬 스토리지에 사용자 이름이 있으면 show greetings
 }
 
